f47baad61904768fa0f50ffd8d111f11e48957fd
magic-onion <alexander.m.truong@gmail.com>
2019-01-09 18:51:08 -0500

    strong params

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -7,6 +7,11 @@ class Api::V1::CommunitiesController < ApplicationController
   end
 
   def create
+    @community = Community.create(community_params)
+    @community.user = User.first
+    @community.save
+    puts "saved"
+    render json: @community
   end
 
   def update
@@ -14,6 +19,26 @@ class Api::V1::CommunitiesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def community_params
+    puts params
+    # thing = {
+    #   :community => {
+    #     :name => params["community"]["name"],
+    #     :rooms => params["community"]["rooms"].to_i,
+    #     :members => params["community"]["members"].to_i,
+    #     :date => Date.parse(params["community"]["date"]).to_date
+    #   }
+    # }
+
+    params["community"]["rooms"] = params["community"]["rooms"].to_i
+    params["community"]["members"] = params["community"]["members"].to_i
+
+    params.require(:community).permit(:name, :start_date)
+  end
+
 
 
 end
