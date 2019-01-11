763c2f7b9baf529345c21606c35347d727873c22
magic-onion <alexander.m.truong@gmail.com>
2019-01-11 11:49:12 -0500

    adjusted serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :status
+  attributes :id, :username, :status
   has_many :communities
   has_many :rooms, through: :communities
   has_many :members, through: :communities
